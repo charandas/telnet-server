48c05e13798c43721f7fe879263abb2ad9200f93
Charandas Batra <charandas@stormforge.io>
2022-04-23 09:24:03 -0700

    Progress

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::io::{
     prelude::Read,
+    prelude::Write,
     ErrorKind
 };
+use std::collections::HashMap;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::sync::mpsc::{
-    self,
-    Sender,
-    Receiver
+use std::sync::{
+    Arc,
+    Mutex,
+    mpsc::{
+        self,
+        Sender,
+        Receiver
+    }
 };
 use std::time::Duration;
 use std::thread::{
@@ -17,34 +23,40 @@ use std::thread::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let mut server = TelnetServer::new();
+    let server = Arc::new(Mutex::new(TelnetServer::new()));
 
     let (tx, rx): (Sender<MessageWrapper>, Receiver<MessageWrapper>) = mpsc::channel();
 
     // TODO: collect this join Handle
-    thread::spawn(|| {
-        for received in rx {
-            println!("Got: {:?}", received.message);
-        }
-    });
+    {
+        let server = Arc::clone(&server);
+        thread::spawn(move || {
+            for received in rx {
+                println!("Got: {:?} from sender: {}", received.message, received.sender_id);
+                let client = server.lock().unwrap().connected_clients.get(&received.sender_id).unwrap();
+
+            }
+        });
+    }
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         println!("Registering new client...");
-        server.register_client(stream, tx.clone())
+        // let server = Arc::clone(&server);
+        server.lock().unwrap().register_client(stream, tx.clone())
     }
 }
 #[derive(Debug)]
 pub struct TelnetServer {
-    connected_clients: Vec<TelnetClient>,
+    connected_clients:  HashMap<u32, TelnetClient>,
     connected_id_counter: u32
 }
 
 #[derive(Debug)]
 pub struct TelnetClient {
-    sender_id: u32,
     join_handle: Option<JoinHandle<()>>,
+    // stream: TcpStream
 }
 
 pub struct MessageWrapper {
@@ -63,21 +75,22 @@ impl TelnetClient {
 impl TelnetServer {
     pub fn new() -> TelnetServer {
         TelnetServer {
-            connected_clients: Vec::new(),
+            connected_clients: HashMap::new(),
             connected_id_counter: 1
         }
     }
     pub fn register_client(&mut self, client: TcpStream, sender: Sender<MessageWrapper>) {
         let sender_id = self.connected_id_counter;
         client.set_nonblocking(true).unwrap();
+
         let join_handle = thread::spawn(move || {
             Self::handle_connection(client, sender_id, sender);
         });
         println!("{:?}", self);
-        self.connected_clients.push(
+        self.connected_clients.insert(
+            sender_id,
             TelnetClient {
-                join_handle: Some(join_handle),
-                sender_id
+                join_handle: Some(join_handle)
             }
         );
         self.connected_id_counter += 1;
@@ -101,6 +114,7 @@ impl TelnetServer {
 
                     } else if num_bytes == 0 {
                         // Stream is closed
+                        drop(sender);
                         println!("Client left. Closing connection...");
                         break;
                     }
