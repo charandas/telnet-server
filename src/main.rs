8eeba1c5b10fae154e97ecbc06673e4dc5aed37f
Charandas Batra <charandas@stormforge.io>
2022-04-23 03:02:02 -0700

    With message wrapper

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use std::io::{
 };
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::sync::mpsc::{
+    self,
+    Sender,
+    Receiver
+};
 use std::time::Duration;
 use std::thread::{
     self,
@@ -14,30 +19,71 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let mut server = TelnetServer::new();
 
+    let (tx, rx): (Sender<MessageWrapper>, Receiver<MessageWrapper>) = mpsc::channel();
+
+    // TODO: collect this join Handle
+    thread::spawn(|| {
+        for received in rx {
+            println!("Got: {:?}", received.message);
+        }
+    });
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        server.register_client(stream)
+
+        println!("Registering new client...");
+        server.register_client(stream, tx.clone())
     }
 }
 #[derive(Debug)]
 pub struct TelnetServer {
+    connected_clients: Vec<TelnetClient>,
+    connected_id_counter: u32
+}
+
+#[derive(Debug)]
+pub struct TelnetClient {
+    sender_id: u32,
+    join_handle: Option<JoinHandle<()>>,
+}
+
+pub struct MessageWrapper {
+    sender_id: u32,
+    message: Vec<u8>,
+}
+
+impl TelnetClient {
+    // fn new(join_handle: Option<JoinHandle<()>>) -> TelnetClient {
+    //     TelnetClient {
+    //         join_handle
+    //     }
+    // }
 }
 
 impl TelnetServer {
     pub fn new() -> TelnetServer {
         TelnetServer {
+            connected_clients: Vec::new(),
+            connected_id_counter: 1
         }
     }
-    pub fn register_client(&mut self, client: TcpStream) {
+    pub fn register_client(&mut self, client: TcpStream, sender: Sender<MessageWrapper>) {
+        let sender_id = self.connected_id_counter;
         client.set_nonblocking(true).unwrap();
-        let join_handle = thread::spawn(|| {
-            Self::handle_connection(client);
+        let join_handle = thread::spawn(move || {
+            Self::handle_connection(client, sender_id, sender);
         });
         println!("{:?}", self);
-        join_handle.join().unwrap();
+        self.connected_clients.push(
+            TelnetClient {
+                join_handle: Some(join_handle),
+                sender_id
+            }
+        );
+        self.connected_id_counter += 1;
     }
 
-    fn handle_connection(mut stream: TcpStream) {
+    fn handle_connection(mut stream: TcpStream, sender_id: u32, sender: Sender<MessageWrapper>) {
         let mut buffer = [0; 1024];
         stream.set_read_timeout(Some(Duration::from_millis(1000))).unwrap();
         loop {
@@ -45,6 +91,14 @@ impl TelnetServer {
                 Ok(num_bytes) => {
                     if num_bytes > 0 {
                         println!("{:?}", buffer.get(0..num_bytes));
+                        match buffer.get(0..num_bytes) {
+                            Some(returned) => sender.send(MessageWrapper {
+                                sender_id,
+                                message: returned.to_vec(),
+                            }).unwrap(),
+                            None => {}
+                        };
+
                     } else if num_bytes == 0 {
                         // Stream is closed
                         println!("Client left. Closing connection...");
@@ -60,10 +114,6 @@ impl TelnetServer {
                     }
                 }
             };
-            /* match stream.peek(&mut buffer) {
-                Err(_) => break,
-                Ok(_) => {}
-            } */
         }
     }
 
