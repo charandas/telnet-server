86e12ba8680a10837c9267a019cbafdd1647ca6d
Charandas Batra <charandas@stormforge.io>
2022-04-24 23:02:25 -0700

    Refactor out rx handler from main

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,13 @@ pub mod telnet_messaging {
     pub mod telnet_client;
 }
 
-use std::io::{
-    prelude::Write
-};
-
 use std::sync::{
-    Arc,
     mpsc::{
         self,
         Sender,
         Receiver
     }
 };
-
-use std::thread;
 use std::net::TcpListener;
 use crate::telnet_messaging::telnet_server::{
     MessageWrapper,
@@ -30,21 +23,7 @@ fn main() {
     let (tx, rx): (Sender<MessageWrapper>, Receiver<MessageWrapper>) = mpsc::channel();
 
     // Register receive handler that will fan-out the responses
-    let cloned_server = Arc::clone(&server.inner);
-    thread::spawn(move || {
-        for received in rx {
-            println!("Got: {:?} from sender: {}", received.message, received.sender_id);
-
-            for (id, client) in cloned_server.lock().unwrap().connected_clients.iter_mut() {
-                if *id != received.sender_id {
-                    client.stream.write(format!("{}: ", received.sender_id).as_bytes()).unwrap();
-                    client.stream.write(&received.message).unwrap();
-                    client.stream.flush().unwrap();
-                }
-            }
-
-        }
-    });
+    server.register_rx_handler(rx);
 
     // Register each client's stream with a unique tx that will pass the data back from their thread into the server receive handler
     for stream in listener.incoming() {
--- src/telnet_messaging/telnet_server.rs
+++ src/telnet_messaging/telnet_server.rs
@@ -1,16 +1,16 @@
 use std::io::{
-    // prelude::Read,
     BufRead,
     BufReader
 };
 use std::collections::HashMap;
+use std::io::prelude::Write;
 use std::net::TcpStream;
-
 use std::thread;
 
 use std::sync::{
     Arc,
     Mutex,
+    mpsc::Receiver
 };
 
 use std::sync::mpsc::Sender;
@@ -58,6 +58,25 @@ impl TelnetServer {
         self.inner.lock().unwrap().connected_id_counter += 1;
 
         sender_id
+    }
+
+    pub fn register_rx_handler(&mut self, rx: Receiver<MessageWrapper>) {
+        let cloned_server = Arc::clone(&self.inner);
+
+        thread::spawn(move || {
+            for received in rx {
+                println!("Got: {:?} from sender: {}", received.message, received.sender_id);
+
+                for (id, client) in cloned_server.lock().unwrap().connected_clients.iter_mut() {
+                    if *id != received.sender_id {
+                        client.stream.write(format!("{}: ", received.sender_id).as_bytes()).unwrap();
+                        client.stream.write(&received.message).unwrap();
+                        client.stream.flush().unwrap();
+                    }
+                }
+
+            }
+        });
     }
 
     fn handle_connection(stream: TcpStream, sender_id: u32, sender: Sender<MessageWrapper>) {
